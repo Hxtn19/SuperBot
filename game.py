0cde9d873d79d429a1e89b16fe67d2d3c9a623db
Hxtn19 <96773789+Hxtn19@users.noreply.github.com>
2021-12-28 19:52:19 +0900

    Update game.py

--- game.py
+++ game.py
@@ -27,3 +27,10 @@ def gamble():
     else:
         print("실패")
         return False
+
+ def coin():
+    coin_face = random.randrange(0,2)
+    if coin_face == 0:
+        return "홀"
+    elif coin_face == 1:
+        return "짝"
